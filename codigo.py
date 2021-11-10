dbe0911444b3a052ff6e99ece78a17f0c1dafb6a
João Carlos Tonon Campi <joaocarloscampi@hotmail.com>
2021-11-10 18:55:14 -0300

    Adicionando leitura de webcam
    Utilizando a biblioteca opencv, estamos capturando os frames da webcam e mostrando em uma janela em tempo real para o usuário.

--- codigo.py
+++ codigo.py
@@ -1,1 +1,29 @@
-print("Hello Workshop!")
+# Biblioteca reconhceimento facial
+import cv2
+
+# Função ouvir_microfone
+
+video_capture = cv2.VideoCapture(0)
+# Cascades
+
+# Flags
+
+while True:
+    ret, frame = video_capture.read()
+
+    teste = cv2.waitKey(1)
+
+    if teste == ord('a'):
+        flagOuvir = True
+    if teste == ord('q'):
+        break
+
+    # Toda a parte do while para reconhecimento facial + voz
+
+    cv2.imshow('video', frame)
+
+
+#ouvir_microfone()
+
+video_capture.release()
+cv2.destroyAllWindows()
